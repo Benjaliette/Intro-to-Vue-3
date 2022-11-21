3be85f092f26648e81dadc5e458a7f6d0746f8f8
Benjamin Liet <benjamin.liet.dev@gmail.com>
2022-11-21 12:17:18 +0100

    create and mount vue app to display datas

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      description: "Nice and warm socks to fight winter. Winter is coming John Snow ..."
+    }
+  }
+})
